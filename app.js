ba150daa90361f1e5bf9afae3d9c928f97c45526
Dominik Loncar <dominikloncar@outlook.com>
2024-01-19 23:07:03 +0100

    Commented out logs for testing purposes.

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
 const num23 = 23;
-console.log(Object.getPrototypeOf(num23));
+//console.log(Object.getPrototypeOf(num23));
 
 function plusTwo(num) {
   return num + 2;
 }
 
-console.log(plusTwo(2));
+//console.log(plusTwo(2));
 
 function greet(whatToSay) {
   return function (name) {
-    console.log(whatToSay + ' ' + name);
+    //console.log(whatToSay + ' ' + name);
   };
 }
 
 var sayHi = greet('Hi');
-sayHi('Dom');
+//sayHi('Dom');
 
 const greetMe = name => `Hi ${name}!`;
-console.log(greetMe('Dom'));
+//console.log(greetMe('Dom'));
 
 let firstTenArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-firstTenArray.map(num => console.log(num));
+//firstTenArray.map(num => console.log(num));
 
 let obj = {
   name: 'Pepec',
   age: '79421',
 };
 
-console.log(obj);
+//console.log(obj);
 
 // Execution context practice.
 
 function b() {
   var myVar;
-  console.log(myVar);
+  //console.log(myVar);
 }
 
 function a() {
   var myVar = 2;
-  console.log(myVar);
+  //console.log(myVar);
   b();
 }
 
 var myVar = 1;
-console.log(myVar);
+//console.log(myVar);
 a();
-console.log(myVar);
+//console.log(myVar);
 
 // Execution context practice #2.
 
 function b2() {
-  console.log(myVar2);
+  //console.log(myVar2);
 }
 
 function a2() {
@@ -66,7 +66,7 @@ a2();
 
 function a3() {
   function b3() {
-    console.log(myVar3);
+    //console.log(myVar3);
   }
 
   var myVar3 = 2;
@@ -82,7 +82,7 @@ for (var i = 0; i < 3; i++) {
   // and store them in funcs
   funcs[i] = function () {
     // each should log its value.
-    console.log('My value: ' + i);
+    //console.log('My value: ' + i);
   };
 }
 for (var j = 0; j < 3; j++) {
