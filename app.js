bbd2511a7fdeeb3992111e6a579f524e3867dd6c
Dominik Loncar <dominikloncar@outlook.com>
2024-01-20 14:51:12 +0100

    Moved prototype examples into a new js file.

--- app.js
+++ app.js
@@ -112,13 +112,3 @@ a.length; // === 10
 function Foo() {
   this.name = 'John Doe';
 }
-
-// Prototype example
-/* Foo has an object property called "prototype"
-   The prototype was created automatically when we declared the function Foo. */
-Foo.hasOwnProperty('prototype'); // true
-
-// Once created, we can assign properties and methods to it
-Foo.prototype.myName = function () {
-  return 'My name is ' + this.name;
-};
